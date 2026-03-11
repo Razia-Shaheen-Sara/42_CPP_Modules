f6c8afbcbb7bc8bef9bb06ca916249d7302a11b1
rshaheen <rshaheen@student.codam.nl>
2026-03-11 16:25:25 +0100

    finished ex01

--- CPP_07/ex01/iter.hpp
+++ CPP_07/ex01/iter.hpp
@@ -4,15 +4,23 @@
 
 #include <iostream>
 
-template <typename T>
+//Requirement:
+//It needs to work for both const and non const reference
+//Solution:
+//*Let the function pointer also be templated */ so it can accept both
+//- void f(T&)
+//- void f(const T&)
+//The Goal: 
+//iter must be able to handle a double, a std::string, or a Fixed class 
+//without changing a single line of the iter function itself.
 
-void iter(T *array, const size_t length, void (*f)(T &))
+template <typename T, typename F>
+void iter(T *array, const size_t length, F function)
 {
-	if (f)
-	{
-		for (size_t i = 0; i < length; i++)
-			f(array[i]);
-	}
+	if (!array)
+        return;
+    for (size_t i = 0; i < length; i++)
+        function(array[i]);
 }
 
 #endif 
--- CPP_07/ex01/main.cpp
+++ CPP_07/ex01/main.cpp
@@ -1,33 +1,32 @@
 #include "iter.hpp"
+#include <iostream>
+#include <string>
 
 template <typename T>
-void printArray(T &element)
+void printElement(T const & x) { std::cout << x << std::endl; }
+
+void toUpper(char & c) { c = toupper(c); }
+
+int main() 
 {
-	std::cout << element << " ";
+    // Test 1: Int array (Non-const)
+    int tab[] = { 0, 1, 2, 3, 4 };
+    iter(tab, 5, printElement<int>);
+
+    // Test 2: String array (Const)
+    const std::string strs[] = { "apple", "banana" };
+    iter(strs, 2, printElement<std::string>);
+
+    // Test 3: Modifying values (Non-const reference)
+    char word[] = "hello";
+    iter(word, 5, toUpper); // No <char> needed here because toUpper isn't a template
+    iter(word, 5, printElement<char>);
+
+    return 0;
 }
 
-template <typename T>
-void addOne(T &element)
-{
-	element++;
-}
-
-int main()
-{
-	void (*fptr)(int &x) = nullptr;
-	int const Carray[4] = {0, 1, 2, 3};
-	int array[4] = {0, 1, 2, 3};
-	char string[] = "abc";
-	
-	::iter(array, 4, fptr);
-	
-	::iter(Carray, 4, printArray);
-	
-	::iter(array, 4, addOne);
-	::iter(array, 4, printArray);
-
-	::iter(string, sizeof(string), printArray);
-	::iter(string, sizeof(string), addOne);
-	::iter(string, sizeof(string), printArray);
-
-}
+//Why <int> or <char>? 
+//Because printElement is a template function, and the compiler needs to know 
+//which version to create before passing it as a parameter.
+//so <> is being used for the template function here
+//ALSO it proves that iter works for all types 
